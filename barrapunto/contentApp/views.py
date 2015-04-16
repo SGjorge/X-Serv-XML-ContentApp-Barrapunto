0c0a8b7fdccaf53da940781af3a828cea58331a2
jorge simon gil <jsimon@epsilon28.aulas.gsyc.es>
2015-04-16 18:44:18 +0200

    barra punto

---
+++ barrapunto/contentApp/views.py
@@ -0,0 +1,34 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseNotFound
+from models import Page
+from xmlbarrapunto import getNews
+from django.core.cache import caches
+
+
+cache = caches['default']
+
+
+def updateNews(request):
+    news = getNews()
+    cache.set('news', news)
+    return HttpResponse("News updated<br>")
+
+
+@csrf_exempt
+def processRequest(request, resource):
+    if request.method == "GET":
+        try:
+            content = Page.objects.get(name=resource)
+            news = cache.get('news')
+            if news == None:
+                news = getNews()
+                cache.set('news', news)
+            return HttpResponse(content.content + news)
+        except Page.DoesNotExist:
+            return HttpResponseNotFound(resource + " not found")
+    elif request.method == "PUT":
+        newContent = Page(name=resource, content=request.body)
+        newContent.save()
+        return HttpResponse(resource + " added to the list")
+    else:
+        return HttpResponse(status=403)
